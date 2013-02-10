ad7711dfc62ee364096079e544c524b06dd34cb7
YannickL <yannick.loriot@gmail.com>
2013-02-10 16:31:31 +0100

    [FIX] iOS 6 support

--- CCControlExtensionExamples/cocos2d-gles20-cccontrol-examples-ios/AppDelegate.m
+++ CCControlExtensionExamples/cocos2d-gles20-cccontrol-examples-ios/AppDelegate.m
@@ -59,7 +59,7 @@
     
 	// set the Navigation Controller as the root view controller
     //	[window_ setRootViewController:rootViewController_];
-	[window_ addSubview:navController_.view];
+	[window_ setRootViewController:navController_];
     
 	// make main window visible
 	[window_ makeKeyAndVisible];
